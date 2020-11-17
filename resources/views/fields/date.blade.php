e63b47ccaef815e25aa758b77eebd1f641410b9e
Zaboev Eduard <zedsh@icloud.com>
2020-11-18 00:32:15 +0300

    Update date.blade.php
    fix date

--- resources/views/fields/date.blade.php
+++ resources/views/fields/date.blade.php
@@ -5,7 +5,7 @@
 ?>
 <div class="form-group">
     <label for="{{$field->getName()}}">{{$field->getTitle()}}</label>
-    <input type="date" class="form-control date @error($field->getName()) is-invalid @enderror" id="{{$field->getName()}}" name="{{$field->getName()}}"
+    <input type="text" class="form-control date @error($field->getName()) is-invalid @enderror" id="{{$field->getName()}}" name="{{$field->getName()}}"
            value="{{$field->getValue()}}">
     @error($field->getName())
     <div class="invalid-feedback">
