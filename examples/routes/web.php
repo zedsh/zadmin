09c5791714dc2b639427dbaa3edb5d75bff0d52e
Eduard Zaboev <zed.shadr@gmail.com>
2022-07-01 00:41:33 +0300

    add route

--- examples/routes/web.php
+++ examples/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\ArtistController;
 
 Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function() {
 
+    // good classic old route definition
     Route::prefix('/artist')->name('artist.')->group(function() {
         Route::get('/list', [ArtistController::class, 'list'])->name('list');
         Route::get('/add', [ArtistController::class, 'add'])->name('add');
@@ -12,5 +13,8 @@ Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function() 
         Route::get('/delete/{id}', [ArtistController::class, 'delete'])->name('delete');
         Route::post('/save', [ArtistController::class, 'save'])->name('save');
     });
+
+    //brilliant new luxury resource route
+    Route::resource('user','UserResourceController');
 
 });
