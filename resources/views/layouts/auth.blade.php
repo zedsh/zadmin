4bdb4d22097d6de17b03abb00bffab2e1ec0ad18
Mikhail <m.uriev@notamedia.com.ru>
2023-03-03 17:49:05 +0300

    styles updates

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>ZAdmin</title>
+    <title>кумпаукп</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/admin_assets/css/bootstrap.min.css" rel="stylesheet">
@@ -27,82 +27,79 @@
             }
         }
     </style>
-    <!-- Custom styles for this template -->
-    <link href="/admin_assets/css/dashboard.css" rel="stylesheet">
-    <link href="/admin_assets/libs/fontawesome/css/all.css" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item" style="margin-right: 10px;">
-                                <a class="nav-link" href="{{ route('login') }}">Войти</a>
-                            </li>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item" style="margin-right: 10px;">
+                            <a class="nav-link" href="{{ route('login') }}">Войти</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('registration') }}">Регистрация</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('registration') }}">Регистрация</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                        @endguest
-                    </ul>
-                </div>
+                        @endif
+                    @else
+                    @endguest
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-    <script src="/admin_assets/js/bootstrap.bundle.min.js"></script>
-    <script src="/admin_assets/libs/tinymce/tinymce.min.js"></script>
-    <script src="/admin_assets/js/slug.js"></script>
-    <script src="/admin_assets/js/flatpickr.js"></script>
-    <script src="/admin_assets/js/flatpickr_ru.js"></script>
-    <script>
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="/admin_assets/js/bootstrap.bundle.min.js"></script>
+<script src="/admin_assets/libs/tinymce/tinymce.min.js"></script>
+<script src="/admin_assets/js/slug.js"></script>
+<script src="/admin_assets/js/flatpickr.js"></script>
+<script src="/admin_assets/js/flatpickr_ru.js"></script>
+<script>
 
-        tinymce.init({
-            selector: '.mce', plugins: ['lists','link','code','table'],
-            toolbar: 'numlist bullist link code',
-            forced_root_block : "",
-            height : "480"
-        });
-    </script>
-    <script>
-        $(document).ready(
-            function(){
-                $(('[data-slug-from]')).each(function(){
-                    var $original = $(this);
-                    var id = $(this).data('slug-from');
-                    $(document).on('keyup', '#' + id, function () {
-                        $original.val(url_slug($(this).val()));
-                    });
+    tinymce.init({
+        selector: '.mce', plugins: ['lists','link','code','table'],
+        toolbar: 'numlist bullist link code',
+        forced_root_block : "",
+        height : "480"
+    });
+</script>
+<script>
+    $(document).ready(
+        function(){
+            $(('[data-slug-from]')).each(function(){
+                var $original = $(this);
+                var id = $(this).data('slug-from');
+                $(document).on('keyup', '#' + id, function () {
+                    $original.val(url_slug($(this).val()));
                 });
+            });
 
-                $('.date').flatpickr({
-                    "locale": "ru",
-                    "dateFormat": "d.m.Y"
-                });
-            }
-        );
-    </script>
+            $('.date').flatpickr({
+                "locale": "ru",
+                "dateFormat": "d.m.Y"
+            });
+        }
+    );
+</script>
 </body>
 </html>
