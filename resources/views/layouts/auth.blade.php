57f3b79f7c26320e3edbd89d9bcd8c737e811f25
Mikhail <m.uriev@notamedia.com.ru>
2023-03-06 10:47:50 +0300

    upd app tab title

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>кумпаукп</title>
+    <title>ZAdmin</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/admin_assets/css/bootstrap.min.css" rel="stylesheet">
--- resources/views/pages/registration/index.blade.php
+++ resources/views/pages/registration/index.blade.php
@@ -7,15 +7,6 @@
                 <div class="card">
                     <div class="card-header">Регистрация</div>
 
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
                     <div class="card-body">
                         <form method="POST" action="{{ route('registration') }}">
                             @csrf
