90206d99f2e3ff816e8098a8e3cdfc506f92dc59
Mikhail <m.uriev@notamedia.com.ru>
2023-03-13 11:20:51 +0300

    upd AdminAuth helper

--- src/Helpers/AdminAuth.php
+++ src/Helpers/AdminAuth.php
@@ -13,24 +13,28 @@ class AdminAuth
 
     public static function routes(array $options = [], string $prefix, string $name_prefix)
     {
-        Route::prefix($prefix)->name($name_prefix . '.')->middleware(['web'])->group(function () use ($options) {
-            if ($options['login'] ?? true) {
-                Route::get('login',
-                    [\zedsh\zadmin\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
-                Route::post('login', [\zedsh\zadmin\Controllers\Auth\LoginController::class, 'login']);
-            }
-            if ($options['registration'] ?? true) {
-                Route::get('registration', [
-                    \zedsh\zadmin\Controllers\Auth\RegisterController::class,
-                    'showRegistrationForm'
-                ])->name('registration');
-                Route::post('registration', [\zedsh\zadmin\Controllers\Auth\RegisterController::class, 'register']);
-            }
-            if ($options['logout'] ?? true) {
-                Route::post('logout',
-                    [\zedsh\zadmin\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-            }
-        });
+        if (!self::checkExistAuthRoutesInApp()) {
+            Route::prefix($prefix)->name($name_prefix . '.')->middleware(['web'])->group(function () use ($options) {
+                if ($options['login'] ?? true) {
+                    Route::get('login',
+                        [\zedsh\zadmin\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+                    Route::post('login', [\zedsh\zadmin\Controllers\Auth\LoginController::class, 'login']);
+                }
+                if ($options['registration'] ?? true) {
+                    Route::get('registration', [
+                        \zedsh\zadmin\Controllers\Auth\RegisterController::class,
+                        'showRegistrationForm'
+                    ])->name('registration');
+                    Route::post('registration', [\zedsh\zadmin\Controllers\Auth\RegisterController::class, 'register']);
+                }
+                if ($options['logout'] ?? true) {
+                    Route::post('logout',
+                        [\zedsh\zadmin\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+                }
+            });
+        }
+
+        return;
     }
 
     public static function checkExistAuthRoutesInApp()
