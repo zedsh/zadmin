fb5788acd665774c70ded355ae613afc3ee99980
Mikhail <m.uriev@notamedia.com.ru>
2023-03-13 17:35:26 +0300

    added create Controller by existing model

--- src/Commands/ZAdminCreateController.php
+++ src/Commands/ZAdminCreateController.php
@@ -5,6 +5,8 @@ namespace zedsh\zadmin\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 
+use function Sodium\add;
+
 class ZAdminCreateController extends Command
 {
     public $signature = 'admin:create {modelName}';
@@ -20,9 +22,68 @@ class ZAdminCreateController extends Command
     {
         $this->createController();
     }
+
     protected function createController()
     {
-        $this->comment($this->argument('modelName'));
+        if ($this->isModelExists()) {
+            $data = file_get_contents(__DIR__ . '/assets/expand/createController/NewController.php');
+            $data = str_replace([
+                'resource_name',
+                'NewController',
+                'protected $modelClass = null;',
+                'use App\Http\Requests\Admin\TagStoreUpdateRequest;',
+                'protected $request = null;'
+            ],
+                [
+                    strtolower($this->argument('modelName')),
+                    $this->argument('modelName').'Controller',
+                    'protected $modelClass = '.$this->argument('modelName').'::class'.';',
+                    'use App\Http\Requests\Admin\\'.$this->argument('modelName').'StoreUpdateRequest;',
+                    'protected $request = '.$this->argument('modelName').'StoreUpdateRequest::class;'
+                ], $data);
+            $fillable = $this->parseModel();
+            $fillableAddEdit = [];
+            foreach ($fillable as $field) {
+                $fillableAddEdit[] = "new TextField('".$field."', '".$field."'),\n";
+            }
+            $data = str_replace("new TextField('name', 'name'),",implode('',$fillableAddEdit), $data);
+            $list = "            (new TextColumn('id', '#'))->setWidth(50)";
+            foreach ($fillable as $field) {
+                if ($field === 'name') {
+                    $list = $list . "
+            (new TextColumn('name', 'name')),\n";
+                }
+            }
+            $data = str_replace("(new TextColumn('id', '#'))->setWidth(50)", $list, $data);
+            dd($data);
+            file_put_contents(app_path('Http/Controllers/Admin/'.$this->argument('modelName').'Controller.php'),$data);
+        }
+    }
+
+
+    protected function isModelExists() {
+        $modelsInDirectory = scandir(app_path() . '/Models');
+        foreach ($modelsInDirectory as $modelName) {
+            if (mb_strpos($modelName, $this->argument('modelName')) !== false && $modelName === $this->argument('modelName').'.php') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function parseModel() {
+        $parsedModel = file_get_contents(app_path() . '/Models/' . $this->argument('modelName') . '.php');
+        $startFillable = strpos($parsedModel,'protected $fillable = [') + 23;
+        $endFillable = strpos($parsedModel,'];');
+        $fillable = mb_substr($parsedModel,$startFillable, ($endFillable-$startFillable));
+        $fillable = ltrim($fillable);
+        $fillable = rtrim($fillable);
+        $fillable = str_replace("\n", "", $fillable);
+        $fillable = str_replace("'", "", $fillable);
+        $fillable = str_replace(" ", "", $fillable);
+        $fillable = explode(',', $fillable);
+        return $fillable;
     }
 
 }
---
+++ src/Commands/assets/expand/createController/NewController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\Admin\TagStoreUpdateRequest;
+use zedsh\zadmin\Fields\TextField;
+use zedsh\zadmin\Lists\Columns\TextColumn;
+
+class NewController extends AdminResourceController
+{
+    protected $modelClass = null;
+    protected $request = null;
+    protected $resourceName = 'resource_name';
+
+    protected $indexTitle = 'Список';
+    protected $editTitle = 'Редактирование';
+    protected $createTitle = 'Создание';
+
+    protected function addEdit($model)
+    {
+        return [
+            new TextField('name', 'name'),
+        ];
+    }
+
+    protected function list(): array
+    {
+        return [
+            (new TextColumn('id', '#'))->setWidth(50),
+        ];
+    }
+}
